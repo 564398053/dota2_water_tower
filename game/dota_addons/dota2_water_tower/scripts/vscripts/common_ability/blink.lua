75cb071a58d14b5658a6628c6f9ae1ff254d1712
lijian <564398053@qq.com>
2018-11-22 00:46:38 +0800

    fix blink

--- game/dota_addons/dota2_water_tower/scripts/vscripts/common_ability/blink.lua
+++ game/dota_addons/dota2_water_tower/scripts/vscripts/common_ability/blink.lua
@@ -4,10 +4,5 @@ blink = class({})
 function blink:OnSpellStart()
     local hCaster = self:GetCaster()
     local hTargetPos = hCaster:GetCursorPosition()
-    
-    local hero = hCaster:GetPlayerOwner():GetAssignedHero()
-    local playerID = hero:GetPlayerID()
-    local player = PlayerResource:GetPlayer( playerID )
-    
-    hero:SetAbsOrigin( hTargetPos )
+    hCaster:SetOrigin( hTargetPos )
 end
