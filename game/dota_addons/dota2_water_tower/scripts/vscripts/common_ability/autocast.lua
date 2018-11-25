21229ba7196622a626323b8634a0382441af1352
lijian <564398053@qq.com>
2018-11-25 20:04:46 +0800

    fix autocast

--- game/dota_addons/dota2_water_tower/scripts/vscripts/common_ability/autocast.lua
+++ game/dota_addons/dota2_water_tower/scripts/vscripts/common_ability/autocast.lua
@@ -1,6 +1,11 @@
 -- autocast the ability
 function AutocastOnAttack( event )
     local ability = event.ability
+    local caster = event.caster
+    local target = event.target
+
+    caster:SetCursorCastTarget(target)
+
     if ability:IsCooldownReady() then
         ability:CastAbility()
     end
