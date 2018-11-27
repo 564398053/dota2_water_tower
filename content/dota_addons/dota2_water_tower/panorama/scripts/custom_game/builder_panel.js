eee8340f2a534a53c8cb639326575bfe5ceffc42
lijian <564398053@qq.com>
2018-11-28 01:55:35 +0800

    panorama ui show tooltip
    great improvement!

---
+++ content/dota_addons/dota2_water_tower/panorama/scripts/custom_game/builder_panel.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function BuildUnit(unitName) {
+    $.Msg( "In function BuildUnit():", unitName );
+}
+
+function ShowToolTip(unitName) {
+    $.Msg( "Compiled Panorama Scripts!" );
+}
