605a28bc24cc1e1fb48d0e3cf4f972ed47040e48
lijian <564398053@qq.com>
2018-11-29 00:56:36 +0800

    implement build unit by PUI.
    greate improvement!
    good job!

--- content/dota_addons/dota2_water_tower/panorama/scripts/custom_game/building_panel.js
+++ content/dota_addons/dota2_water_tower/panorama/scripts/custom_game/building_panel.js
@@ -4,17 +4,33 @@ $.Msg("load building_panel.js start");;
 
 function BuildUnit(unitName) {
     $.Msg( "In function BuildUnit():", unitName );
+
+    $.DispatchEvent("DOTAHideAbilityTooltip");
+    $("#building_panel").ToggleClass("building-panel--hide");
+
+    var order = {
+        AbilityIndex : Entities.GetAbility( Players.GetPlayerHeroEntityIndex( Players.GetLocalPlayer() ), 2 ),
+        QueueBehavior : OrderQueueBehavior_t.DOTA_ORDER_QUEUE_NEVER,
+        ShowEffects : false,
+        OrderType : dotaunitorder_t.DOTA_UNIT_ORDER_CAST_NO_TARGET
+    };
+    var abilityBehavior = Abilities.GetBehavior( order.AbilityIndex );
+    if ( abilityBehavior & DOTA_ABILITY_BEHAVIOR.DOTA_ABILITY_BEHAVIOR_POINT )
+    {
+        order.OrderType = dotaunitorder_t.DOTA_UNIT_ORDER_CAST_POSITION;
+        order.Position = GetMouseCastPosition( order.AbilityIndex );
+    }
+
+    Game.PrepareUnitOrders( order );
 }
 
 // JS directly interact with elements in game.
 function OnExecuteAbility1ButtonPressed( cmdName )
 {
     $.Msg( "ExecuteAbility1 as " + cmdName );
-    var panel = $("#building_panel");
-
-    $.Msg( "#building_panel", panel );
     // toggle panel
-    panel.ToggleClass("building-panel--hide");
+    $("#building_panel").ToggleClass("building-panel--hide");
+
     // var order = {
     //     AbilityIndex : Entities.GetAbility( Players.GetPlayerHeroEntityIndex( Players.GetLocalPlayer() ), 1 ),
     //     QueueBehavior : OrderQueueBehavior_t.DOTA_ORDER_QUEUE_NEVER,
